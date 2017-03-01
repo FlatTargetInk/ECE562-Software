47bef0f686853add024bbd102a4853e9a1bc8f4f
M. Phil Lowney <mlowney@umassd.edu>
2017-03-01 08:15:12 -0500

    Add setup.py and add to gitignore

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+        # Metadata
+        name='560software',
+        author='ECE-560 Group',
+        author_email='',
+        version='0.0.1',
+        description='FPGA virtual memory visualizer',
+        url='https://github.com/FlatTargetInk/ECE562-Software',
+        license='MIT',
+        packages=find_packages(),
+        )
